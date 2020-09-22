0b515f051979382c22e0612adb0079bdf30564d0
Wander <wanderdasouza@gmail.com>
2020-09-22 19:57:05 -0300

    WIP: fetch data from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,19 @@
   <div>
     <div id="searchBar">
       <img src alt="Lupa" />
-      <input type="text" @keydown.enter="submitSearch" placeholder="Buscar por oportunidades..." />
+      <input
+        type="text"
+        @keydown.enter="submitSearch"
+        placeholder="Buscar por oportunidades..."
+      />
     </div>
 
     <div class="card" v-for="(result, index) in results" :key="index">
       <div class="card-container">
         <div class="opportunity-info">
-          <p>Titulo: {{result.title}}</p>
-          <p>Autor: {{result.author}}</p>
-          <p>{{result.desc}}</p>
+          <p>Titulo: {{ result.title }}</p>
+          <p>Autor: {{ result.author }}</p>
+          <p>{{ result.desc }}</p>
         </div>
 
         <div class="tags-container">
@@ -19,12 +23,10 @@
           </div>
         </div>
       </div>
-      
+
       <button class="btn-more-info">Saiba Mais</button>
     </div>
-    <div>
-
-    </div>
+    <div></div>
   </div>
 </template>
 
@@ -33,54 +35,54 @@
 export default {
   name: "Home",
   data: () => ({
-    results: [{
-      title: "Inter",
-      author: "Guerrero",
-      desc: "fasdf",
-      tags: [
-        "Estágio",
-        "IC",
-      ]
-    }, 
-    {
-      title: "AIFjaoi",
-      author: "lorem",
-      desc: "ipsum",
-      tags: [
-        "IC",
-        "Monitoria",
-      ]
-    }]
+    results: [
+      {
+        title: "Inter",
+        author: "Guerrero",
+        desc: "fasdf",
+        tags: ["Estágio", "IC"]
+      },
+      {
+        title: "AIFjaoi",
+        author: "lorem",
+        desc: "ipsum",
+        tags: ["IC", "Monitoria"]
+      }
+    ]
   }),
   methods: {
     submitSearch() {
-      console.log("Thiago Otário");
+      
     }
+  },
+  async created() {
+    const { body } = await fetch(
+      "https://my-json-server.typicode.com/jooaodanieel/db-devboost/oportunities"
+    );
+    console.log(await body.toString());
   }
 };
 </script>
 <style scoped>
+.card {
+  border: 1px solid red;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+}
 
-  .card {
-    border: 1px solid red;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-  }
+.card-container {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .card-container {
-    display: flex;
-    justify-content: space-between;
-  }
+.tag {
+  background-color: aquamarine;
+}
 
-  .tag {
-    background-color: aquamarine;
-  }
-
-  .btn-more-info {
-    background-color: blueviolet;
-    width: 300px;
-    align-self: flex-end;
-  }
-
+.btn-more-info {
+  background-color: blueviolet;
+  width: 300px;
+  align-self: flex-end;
+}
 </style>
