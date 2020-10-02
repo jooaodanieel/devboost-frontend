7e44239fcd5d4abd772f5308c636ead928caae48
luizcarlosdk <luizcarlosdk@usp.br>
2020-10-02 20:13:21 -0300

    WIP: Add new opportunities scaffold
    Signed-off-by: luizcarlosdk <luizcarlosdk@usp.br>

---
+++ src/views/NewOpportunities.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form action="">
+      <label for=""></label>
+      <input type="text" v-model="title"><br/>
+      <label for=""></label>
+      <input type="text" v-model="author"><br/>
+      <label for=""></label>
+      <input type="text" v-model="description"><br/>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data:()=>({
+    title: "",
+    author: "",
+    description: ""
+  })
+}
+</script>
+
+<style>
+
+</style>
