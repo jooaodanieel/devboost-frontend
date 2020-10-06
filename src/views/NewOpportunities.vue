158872297591b078b4d3f823a63b8ad4ef794385
Thiago Guerrero <tgbalera@hotmail.com>
2020-10-06 20:46:45 -0300

    style new opportunity forms
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- src/views/NewOpportunities.vue
+++ src/views/NewOpportunities.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    <form>
-      <label for="title">Title: </label>
-      <input name="title" type="text" v-model="title" /><br />
-      <label for="author">Author: </label>
-      <input name="author" type="text" v-model="author" /><br />
-      <label for="description">Description:</label>
-      <input name="description" type="text" v-model="description" /><br />
-      <button type="button" @click="registerOpportunities">
-        Register
-      </button>
-    </form>
+  <div class="new">
+    <div class="form">
+      <form>
+        <div class="input-field">
+          <label for="title">Título </label>
+          <input name="title" type="text" v-model="title" /><br />
+        </div>
+        <div class="input-field">
+          <label for="author">Autor </label>
+          <textarea name="author" type="text" v-model="author" /><br />
+        </div>
+        <div class="input-field">
+          <label for="description">Descrição</label>
+          <textarea name="description" type="text" v-model="description" /><br />
+        </div>
+      </form>
+      <div class="buttons">
+        <button type="button" @click="registerOpportunities">
+          Cadastrar
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,4 +51,72 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+
+.new {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.form{
+  background-color: #9AD8F2;
+  margin: 50px auto;
+  padding: 20px;
+  width: 80%;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.form form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  width: 65%;
+}
+
+.input-field {
+  display: flex;
+  width: 65%;
+  flex-direction: column;
+}
+
+.input-field input {
+  border: none;
+  border-radius: 6px;
+  height: 2rem;
+}
+
+.input-field textarea{
+  resize: vertical;
+  border: none;
+  height: 100px;
+  min-height: 100px;
+  max-height: 300px;
+  border-radius: 6px;
+}
+
+.buttons{
+  width: 25%;
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 18px;
+}
+.buttons button{
+  background-color: #673AB7;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  width: 80%;
+  height: 60px;
+  font-size: 1.5rem;
+  font-weight: 700;
+  transition: 0.2s;
+}
+
+.buttons button:hover{
+  background-color: #57309b;
+}
+</style>
