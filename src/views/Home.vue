cbe03de9160659b8dadc0ec0d90cca39d5c01c80
Gabriel Mota <gabrielfmota134@gmail.com>
2020-10-13 20:51:41 -0300

    Fix filter issues Signed-off-by: Édio Cerati Neto <edioceratineto@gmail.com> Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br> Signed-off-by: Wander da Souza <wanderdasouza@gmail.com> Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,11 @@
   <div>
     <div id="titleSearchBar">
       <img src alt="Lupa" />
-      <input
-        type="text"
-        v-model="title"
-        placeholder="Buscar pelo título..."
-      />
+      <input type="text" v-model="title" placeholder="Buscar pelo título..." />
     </div>
     <div id="authorSearchBar">
       <img src alt="Lupa" />
-      <input
-        type="text"
-        v-model="author"
-        placeholder="Buscar por autor..."
-      />
+      <input type="text" v-model="author" placeholder="Buscar por autor..." />
     </div>
     <div id="descriptionSearchBar">
       <img src alt="Lupa" />
@@ -58,8 +50,7 @@
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 // @ == v-on:
 export default {
@@ -73,25 +64,26 @@ export default {
   }),
   methods: {
     async submitSearch() {
-      const response = await axios.get("http://localhost:3000/opportunities", 
-      {params: 
-      {title: this.title,
-       description: this.description,
-       author: this.author
-       }});
+      const response = await axios.get("http://localhost:3000/opportunities", {
+        params: {
+          title: this.title,
+          description: this.description,
+          author: this.author,
+        },
+      });
       this.filtered = response.data.opportunities;
-    }
+    },
   },
   computed: {
     filteredOpportunities() {
       return this.filtered;
-    }
+    },
   },
   async created() {
     const response = await axios.get("http://localhost:3000/opportunities");
     const body = await response.data;
     this.filtered = body.opportunities;
-  }
+  },
 };
 </script>
 <style scoped>
