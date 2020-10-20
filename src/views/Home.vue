0c088827dc874302815dd2a3eab1914938929215
Wander Souza <wanderdasouza@gmail.com>
2020-10-20 20:38:08 -0300

    feat: style filter
    Signed-off-by: Édio Cerati Neto <edioceratineto@gmail.com>
    Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br>
    Signed-off-by: Wander da Souza <wanderdasouza@gmail.com>
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,62 @@
 <template>
   <div>
-    <div class="upperSearchBars">
-      <div class="titleSearchBar">
-        <img src="https://svgsilh.com/png-512/1093184.png" alt="Lupa" />
-        <input
-          v-on:keyup.enter="submitSearch"
-          type="text"
-          v-model="title"
-          placeholder="Buscar pelo título..."
-        />
-      </div>
-      <div class="authorSearchBar">
-        <img src="https://svgsilh.com/png-512/1093184.png" alt="Lupa" />
-        <input
-          v-on:keyup.enter="submitSearch"
-          type="text"
-          v-model="author"
-          placeholder="Buscar por autor..."
-        />
-      </div>
-    </div>
-    <div class="descriptionSearchBar">
-      <img src="https://svgsilh.com/png-512/1093184.png" alt="Lupa" />
-      <textarea
-        v-on:keydown.enter.prevent="submitSearch"
-        type="text"
-        v-model="description"
-        placeholder="Buscar pela descrição..."
-      />
-    </div>
-    <div class="submitSearch">
-      <button @click="submitSearch">Procurar</button>
-    </div>
-
-    <div
-      class="card"
-      v-for="(opportunity, index) in filteredOpportunities"
-      :key="index"
-    >
-      <div class="card-container">
-        <div class="opportunity-info">
-          <p>Titulo: {{ opportunity.title }}</p>
-          <p>Autor: {{ opportunity.author }}</p>
-          <p>{{ opportunity.description }}</p>
+    <section class="mainContent">
+      <div class="upperSearchBars">
+        <div class="titleSearchBar box">
+          <img src="https://svgsilh.com/png-512/1093184.png" alt="Lupa" />
+          <input
+            v-on:keyup.enter="submitSearch"
+            type="text"
+            v-model="title"
+            placeholder="Buscar pelo título..."
+          />
         </div>
+        <div class="authorSearchBar box">
+          <img src="https://svgsilh.com/png-512/1093184.png" alt="Lupa" />
+          <input
+            v-on:keyup.enter="submitSearch"
+            type="text"
+            v-model="author"
+            placeholder="Buscar por autor..."
+          />
+        </div>
+      </div>
+      <div class="descriptionSearchBar box">
+        <img src="https://svgsilh.com/png-512/1093184.png" alt="Lupa" />
+        <textarea
+          v-on:keydown.enter.prevent="submitSearch"
+          type="text"
+          v-model="description"
+          placeholder="Buscar pela descrição..."
+        />
+      </div>
 
-        <div class="tags-container">
-          <div
-            class="tag"
-            v-for="(tag, index) in opportunity.tags"
-            :key="index"
-          >
-            {{ tag }}
+      <div
+        class="card box"
+        v-for="(opportunity, index) in filteredOpportunities"
+        :key="index"
+      >
+        <div class="card-container">
+          <div class="opportunity-info">
+            <p>Titulo: {{ opportunity.title }}</p>
+            <p>Autor: {{ opportunity.author }}</p>
+            <p>{{ opportunity.description }}</p>
+          </div>
+
+          <div class="tags-container">
+            <div
+              class="tag"
+              v-for="(tag, index) in opportunity.tags"
+              :key="index"
+            >
+              {{ tag }}
+            </div>
           </div>
         </div>
-      </div>
 
-      <button class="btn-more-info">Saiba Mais</button>
-    </div>
-    <div></div>
+        <button class="btn-more-info">Saiba Mais</button>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -73,7 +71,7 @@ export default {
     filtered: [],
     title: "",
     description: "",
-    author: "",
+    author: ""
   }),
   methods: {
     async submitSearch() {
@@ -81,30 +79,38 @@ export default {
         params: {
           title: this.title,
           description: this.description,
-          author: this.author,
-        },
+          author: this.author
+        }
       });
       this.filtered = response.data.opportunities;
-    },
+    }
   },
   computed: {
     filteredOpportunities() {
       return this.filtered;
-    },
+    }
   },
   async created() {
     const response = await axios.get("http://localhost:3000/opportunities");
     const body = await response.data;
     this.filtered = body.opportunities;
-  },
+  }
 };
 </script>
 <style scoped>
+
+.mainContent {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .card {
-  border: 1px solid red;
   margin-top: 10px;
   display: flex;
   flex-direction: column;
+  width: 100%;
 }
 
 .card-container {
@@ -112,41 +118,63 @@ export default {
   justify-content: space-between;
 }
 
+.box {
+  display: flex;
+  padding: 10px;
+  background-color: #9ad8f2;
+  border-radius: 4px;
+}
+
 .tag {
   background-color: aquamarine;
 }
 
+.upperSearchBars input {
+  padding-left: 10px;
+  border: none;
+  outline: none;
+  background-color: inherit;
+  width: 100%;
+}
+
 .btn-more-info {
   background-color: blueviolet;
-  width: 300px;
+  width: 30%;
   align-self: flex-end;
 }
 
 .upperSearchBars {
   display: flex;
-  align-items: center;
-  margin: 20px;
+  width: 100%;
+  margin: 20px 0px;
 }
 
 .titleSearchBar {
-  background-color: #9AD8F2;
+  width: 100%;
+  background-color: #9ad8f2;
   margin-right: 20px;
 }
 
 .authorSearchBar {
-  background-color: #9AD8F2;
+  width: 100%;
+  background-color: #9ad8f2;
 }
 
 .descriptionSearchBar {
-  background-color: #9AD8F2;
+  width: 100%;
+  background-color: #9ad8f2;
 }
 
 textarea {
+  padding-left: 10px;
+  border: none;
+  background-color: inherit;
+  outline: none;
+  width: 97%;
   resize: none;
-  width: 500px;
 }
 
-img {
-  width: 20px;
+.mainContent img {
+  height: 1rem;
 }
 </style>
