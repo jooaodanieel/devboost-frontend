f918897619d703012d9f910caa49edf10ce8351c
Thiago Guerrero <tgbalera@hotmail.com>
2020-10-27 19:24:24 -0300

    WIP: Starting componentization
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

---
+++ src/components/Form.vue
@@ -0,0 +1,125 @@
+<template>
+  <form>
+    <p>Tags:</p>
+    <div class="checkbox-wrapper">
+      <div class="checkbox-field" v-for="(tag, index) in tags" :key="index">
+        <input
+          :name="tag"
+          type="checkbox"
+          :value="tag"
+          v-model="selectedTags"
+        />
+        <label :for="tag">{{ tag }}</label>
+      </div>
+      <div class="checkbox-field">
+        <input name="other" v-model="checked" type="checkbox" />
+        <label class="placeholder" v-if="!checked" for="other">Outro</label>
+        <input
+          :style="inputHidden"
+          class="checkbox-text"
+          type="text"
+          @keydown.enter="createCheckbox"
+        />
+      </div>
+    </div>
+    <div class="input-field">
+      <label for="image">Imagem </label>
+      <input name="image" type="file" @change="handleChange" /> <br />
+      <img v-if="image != undefined" class="imgPreview" :src="imgSrc" />
+    </div>
+    <div class="input-field">
+      <label for="title">Título </label>
+      <input name="title" type="text" v-model="title" /><br />
+    </div>
+    <div class="input-field">
+      <label for="summary">Resumo </label>
+      <textarea name="summary" type="text" v-model="summary" /><br />
+    </div>
+    <div class="input-field">
+      <label for="description">Descrição</label>
+      <textarea name="description" type="text" v-model="description" /><br />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ["tags", "selectedTags"],
+  methods: {
+    createCheckbox(e) {
+      console.log(e.target.value)
+      this.tags.push(e.target.value);
+      this.selectedTags.push(e.target.value);
+      e.target.value = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 65%;
+}
+
+.checkbox-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+}
+
+.checkbox-field {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 10px 5px 0;
+}
+
+.checkbox-field label {
+  margin: 0 3px;
+}
+
+.checkbox-field .placeholder {
+  opacity: 50%;
+}
+
+.checkbox-field .checkbox-text {
+  margin-left: 3px;
+  padding-left: 5px;
+  background: none;
+  border: 1px solid black;
+  outline: none;
+  width: 100px;
+  height: 25px;
+  border-radius: 6px;
+}
+
+.input-field {
+  display: flex;
+  width: 65%;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+.input-field input {
+  border: none;
+  border-radius: 6px;
+  height: 2rem;
+}
+
+.input-field textarea {
+  resize: vertical;
+  border: none;
+  height: 100px;
+  min-height: 100px;
+  max-height: 300px;
+  border-radius: 6px;
+}
+
+.imgPreview {
+  max-width: 400px;
+  max-height: 400px;
+}
+</style>
