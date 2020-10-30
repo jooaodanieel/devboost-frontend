6dd7280e3ab8baa2c824df4741d331fce21f1aaf
Raphael <raphaelrbr@usp.br>
2020-10-30 20:06:12 -0300

    WIP: add tabs-menu

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
+    <Tabs />
     <router-view />
+    
   </div>
 </template>
 
+<script>
+import Tabs from "@/components/Tabs.vue";
+export default {
+  components: {
+    Tabs,  
+  },
+};
+</script>
 <style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 </style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="tabs">
+      <div id="tabs-menu">
+          <button>Ver Oportunidades</button>
+          <button>Cadastar oportunidades</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+    .tabs{
+        display: flex;
+    }
+</style>
+
