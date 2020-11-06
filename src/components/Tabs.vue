6077382373c83891a78581baaaf293944977f8ae
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-11-06 19:55:08 -0300

    finish tabs
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Signed-off-by: Raphael <raphaelrbr@usp.br>
    Signed-off-by: luizcarlosdk <luizcarlosdk@usp.br>
    Signed-off-by: Leonardo Costa Santos <santos.leocosta@gmail.com>

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="tabs">
-    <!-- <a :class="underline[0]" id="home" @click="redirect">Ver Oportunidades</a>
-    <a :class="underline[1]" id="newOpportunity" @click="redirect"
-      >Cadastar oportunidades</a
-    > -->
-    <router-link to="/" tag="button" id="home" exact-active-class="item-selected">Ver oportunidade</router-link>
-    <router-link to="/opportunities/new" tag="button" id="newOpportunity" exact-active-class="item-selected">Cadastar oportunidades</router-link>
+    <router-link
+      to="/"
+      tag="button"
+      id="home"
+      exact-active-class="item-selected"
+      >Ver oportunidade</router-link
+    >
+    <router-link
+      to="/opportunities/new"
+      tag="button"
+      id="newOpportunity"
+      exact-active-class="item-selected"
+      >Cadastar oportunidades</router-link
+    >
   </div>
 </template>
 <script>
-
 export default {
   methods: {
     redirect(e) {
@@ -22,7 +29,7 @@ export default {
         this.path = "new_opportunities";
       }
     }
-  },
+  }
 };
 </script>
 <style scoped>
@@ -38,7 +45,6 @@ export default {
   outline: none;
   border: none;
   font-size: 20px;
-  padding: 3px;
   text-decoration: none;
   cursor: pointer;
   opacity: 70%;
@@ -46,17 +52,15 @@ export default {
 .item-selected {
   color: #0049e5;
   font-weight: bold;
-  
   opacity: 100%;
 }
 
-.item-selected:after{
+.item-selected:after {
   content: "";
-  display:block;
-  margin:0 auto;
-  width:50%;
+  display: block;
+  margin: 0 auto;
+  padding: 2px;
+  width: 70%;
   border-bottom: 3px solid #0049e5;
 }
-
-
 </style>
