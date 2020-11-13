8f42330f1664d0cddbe7d97ccebb4d98b4000a62
Leonardo Costa Santos <santos.leocosta@gmail.com>
2020-11-13 20:31:16 -0300

    WIP: add dropdown options
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Signed-off-by: Raphael <raphaelrbr@usp.br>
    Signed-off-by: luizcarlosdk <luizcarlosdk@usp.br>
    Signed-off-by: Leonardo Costa Santos <santos.leocosta@gmail.com>

--- src/components/DropdownSelect.vue
+++ src/components/DropdownSelect.vue
@@ -1,39 +1,59 @@
 <template>
-<div class="wrapper" style="background: orange">
-  <div class="display" >
-    <div class="tag" v-for="(tag, index) in tags" :key="index">
-      {{ tag }}
+  <div class="dropdown">
+    <div class="wrapper" style="background: orange">
+      <div class="display">
+        <div class="tag" v-for="(tag, index) in tags" :key="index">
+          {{ tag }}
+        </div>
+      </div>
+      <div>
+        button
+      </div>
+    </div>
+    <div class="list">
+      <div v-for="(tag, index) in tags" :key="index">
+        {{ tag }}
+      </div>
     </div>
   </div>
-  <div>
-    button
-  </div>
-</div>
-  
 </template>
 
 <script>
-export default{
+export default {
   props: ["tags"]
-}
+};
 </script>
 
 <style scoped>
-  .wrapper {
-    display:flex;
+.wrapper {
+  display: flex;
+}
 
-  }
-  .display {
-    display:flex;
-    align-items: center;
-    flex:1;
-    overflow:auto;
-    font-size: 12px;
-  }
-  .tag {
-    margin:3px;
-    padding: 2px 4px;
-    background-color:rgb(202, 193, 193);
-    border-radius:10px;
-  }
+.dropdown {
+  background-color: green !important;
+  width: 100%;
+}
+
+.display {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  overflow: auto;
+  font-size: 12px;
+}
+
+.tag {
+  margin: 3px;
+  padding: 2px 4px;
+  background-color: rgb(202, 193, 193);
+  border-radius: 10px;
+}
+
+.list {
+  position: absolute;
+  background-color: blue;
+  z-index: 1;
+  height: 1em;
+  width: 20em;
+}
 </style>
