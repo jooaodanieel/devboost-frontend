682e22b0efb825f9598656bfc457d46b32a87e87
luizcarlosdk <luizcarlosdk@usp.br>
2020-11-13 19:56:21 -0300

    WIP: start tags dropdown
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Signed-off-by: Raphael <raphaelrbr@usp.br>
    Signed-off-by: luizcarlosdk <luizcarlosdk@usp.br>
    Signed-off-by: Leonardo Costa Santos <santos.leocosta@gmail.com>

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -24,7 +24,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .box {
   display: flex;
   padding: 10px;
--- src/components/DropdownSelect.vue
+++ src/components/DropdownSelect.vue
@@ -1,10 +1,15 @@
 <template>
-  <div style="background: blue">
-    <div v-for="(tag, index) in tags" :key="index">
+<div class="wrapper" style="background: orange">
+  <div class="display" >
+    <div class="tag" v-for="(tag, index) in tags" :key="index">
       {{ tag }}
     </div>
-    adfsdafsfdagsdasgdfbagf
   </div>
+  <div>
+    button
+  </div>
+</div>
+  
 </template>
 
 <script>
@@ -14,5 +19,21 @@ export default{
 </script>
 
 <style scoped>
-  
+  .wrapper {
+    display:flex;
+
+  }
+  .display {
+    display:flex;
+    align-items: center;
+    flex:1;
+    overflow:auto;
+    font-size: 12px;
+  }
+  .tag {
+    margin:3px;
+    padding: 2px 4px;
+    background-color:rgb(202, 193, 193);
+    border-radius:10px;
+  }
 </style>
